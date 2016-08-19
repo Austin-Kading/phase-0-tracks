581b75ffa8323fa68dd071eb1c8327e10a75e775
Austin Kading <austinekading@gmail.com>
2016-08-19 11:37:03 -0700

    Added method that creates 50 instances to a class and stores them.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,9 +5,10 @@
 class Santa
   def initialize(gender, ethnicity)
     @gender 
-    @ethnicity 
+    @ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0 
+    @age = 0  
+    
   end
 # method that outputs what santa is saying
 def speak
@@ -19,17 +20,20 @@ def eat_milk_and_cookies(cookie)
   end
 end
 # Set empty santa array to get personal santa input from user
-santa_clause = []
-puts "Yo, what's your gender"
-gen = gets.chomp
-puts "And what's your ethnicity"
-eth = gets.chomp
-santa_clause = Santa.new(gen, eth)
 
-# gives examples of multiple instances for user to input as santa
+
+santa_clause = []
+  puts "Yo, what's your gender"
+gen = gets.chomp
+  puts "And what's your ethnicity"
+eth = gets.chomp
+# santa_clause << Santa.new(gen, eth)
+
+
+# # gives examples of multiple instances for user to input as santa
 genders = ["male", "female", "agender", "bigender", "N/A"]
 ethnicities = ["white", "black", "latino", "japanese", "korean", "N/A"]
 genders.length.times do |i|
   santa_clause << Santa.new(genders[i], ethnicities[i])
 end
-
+p santa_clause
