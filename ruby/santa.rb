a8ccdcd70d6f7f85188de9e77664960c710af791
Austin Kading <austinekading@gmail.com>
2016-08-20 19:37:07 -0700

    Santa project finished with empty array santa with genders, ethnicities and age.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,65 +3,71 @@
 
 
 class Santa
+  attr_reader :age, :ethnicity
+  attr_accessor :gender
+
   def initialize(gender, ethnicity)
-    @gender 
-    @ethnicity
+    @gender = gender
+    @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0  
-    
+    @age = 0
   end
 # method that outputs what santa is saying
-def speak
-    puts "Ho, ho, ho! Haaaappy Holidays!"
-end
-# method that outputs a string
-def eat_milk_and_cookies(cookie)
-  puts "That was a good #{cookie}!"
+  def speak
+      puts "Ho, ho, ho! Haaaappy Holidays!"
+  end
+  # method that outputs a string
+  def eat_milk_and_cookies(cookie)
+    puts "That was a good #{cookie}!"
   end
 
-# Setter method that adds a year to santa
-def celebrate_birthday
-  @age += 1
-end
-
-# Setter method
-def get_mad_at(reindeer_name)
-  if i = @reindeer_ranking.index(reindeer_name)
-    @reindeer_ranking.delete_at(i)
-    @reindeer_ranking << reindeer_name
+  # Setter method that adds a year to santa
+  def celebrate_birthday
+    @age += 1
   end
 
-  def gender = (new_gender)
-    @gender = new_gender
+  # Setter method
+  def get_mad_at(reindeer_name)
+    if i = @reindeer_ranking.index(reindeer_name)
+      @reindeer_ranking.delete_at(i)
+      @reindeer_ranking << reindeer_name
+    end
+  end
+
+  def gender 
+      @gender = gender
+  end
+
+  def ethnicity
+    @ethnicity = ethnicity
+  end
+end
+  santa = Santa.new("male", "white")
+  santa.speak
+  santa.eat_milk_and_cookies("snickerdoodle")
+
+  # Set empty santa array to get personal santa input from user
+
+
+  santa_clause = []
+
+#   # gives examples of multiple instances for user to input as santa
+  genders = ["male", "female", "agender", "bigender", "N/A"]
+  ethnicities = ["white", "black", "latino", "japanese", "korean", "N/A"]
+
+  genders.length.times do |i|
+    santa_clause << Santa.new(genders[i], ethnicities[i])
 end
 
-santa = Santa.new("male", "white")
-santa.speak
-santa.eat_milk_and_cookies
+  ###############################
 
-# Set empty santa array to get personal santa input from user
+  # Release 4 making tons of santas 
 
-
-santa_clause = []
-  puts "Yo, what's your gender"
-gen = gets.chomp
-  puts "And what's your ethnicity"
-eth = gets.chomp
-# santa_clause << Santa.new(gen, eth)
-
-
-<<<<<<< HEAD
-# gives examples of multiple instances for user to input as santa
-=======
-# # gives examples of multiple instances for user to input as santa
->>>>>>> master
-genders = ["male", "female", "agender", "bigender", "N/A"]
-ethnicities = ["white", "black", "latino", "japanese", "korean", "N/A"]
-
-santa_clause.length.times do |i|
-  santa_clause << Santa.new(genders[i], ethnicities[i])
+  genders = ["male", "female", "agender", "bigender", "N/A"]
+  ethnicities = ["white", "black", "latino", "japanese", "korean", "N/A"]
+  50.times do |santa|
+    santa_clause << Santa.new(genders.shuffle[santa], ethnicities.shuffle[santa])
 end
-<<<<<<< HEAD
-=======
-p santa_clause
->>>>>>> master
+
+
+  p santa_clause
