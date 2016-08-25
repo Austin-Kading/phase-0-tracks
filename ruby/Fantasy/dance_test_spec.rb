852bb3af6b28be44cec56a3f60fe1fe838cdc0e0
Austin Kading <austinekading@gmail.com>
2016-08-25 16:04:37 -0700

    commiting

--- ruby/Fantasy/dance_test_spec.rb
+++ ruby/Fantasy/dance_test_spec.rb
@@ -31,10 +31,10 @@ it "bows to a partner" do
   expect(dancer.card).to eq ["Mikhail Baryshnikov", "Anna Pavlova"]
 end
 
-# it "can start the next dance in the queue" do
-#   dancer.queue_dance_with("Mikhail Baryshnikov")
-#   dancer.queue_dance_with("Anna Pavlova")
-#   expect(dancer.begin_next_dance).to eq "Now dancing with Mikhail Baryshnikov."
-#   expect(dancer.card).to eq ["Anna Pavlova"]
-#   end
+it "can start the next dance in the queue" do
+  dancer.queue_dance_with("Mikhail Baryshnikov")
+  dancer.queue_dance_with("Anna Pavlova")
+  expect(dancer.begin_next_dance).to eq "Now dancing with Mikhail Baryshnikov."
+  expect(dancer.card).to eq ["Anna Pavlova"]
+  end
 end
