e3b4cec45412bb2973089c03ae72909c56283906
Austin Kading <austinekading@gmail.com>
2016-09-14 16:04:36 -0700

    added vampire logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,72 +1,79 @@
-#!/usr/bin/ruby
-#vampire survey
-puts "How many employees will be processed?"
-number_of_employees = gets.chomp.to_i
+# Gathering employee data
 
-while number_of_employees >= 0
-  print "What is your name? "
+  puts "How many employees would you like to process?"
+  num_of_employees = gets.chomp.to_i
+
+  while num_of_employees >= 0
+  print "What is your name? \n"
   name = gets.chomp
 
-  print "How old are you?"
+  print "How old are you? \n"
   age = gets.chomp.to_i
 
-  print "What year were you born? "
+  print "What year were you born?\n"
   year_born = gets.chomp.to_i
 
-  print "Our company cafeteria serves garlic bread. Should we order some for you?(y/n) "
-  garlic_eater = gets.chomp
+  print "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)\n"
+  likes_garlic = gets.chomp
 
-  if garlic_eater == "y"
-    garlic_eater = true
-  elsif garlic_eater == "n"
-    garlic_eater = false
-  else 
-    abort "Please choose y or n"
-  end
+    if likes_garlic == "y"
+      likes_garlic = true
+    elsif likes_garlic == "n"
+      likes_garlic = false
+    else
+      abort "Please choose (y or n)"
+    end
 
-  print "Would you like to enroll in the company's health insurance?(y/n) "
-  insurance = gets.chomp
+  print "Would you like to enroll in the company’s health insurance? (y/n)\n"
+  insurance_enroll = gets.chomp
 
-  #This is will allow a "y" or "n" input
-  
-  if insurance == "y"
-    insurance = true
-  elsif insurance == "n"
-    insurance = false
-  else
-    abort "Please pick y or n"
-  end
+    if insurance_enroll == "y"
+      insurance_enroll = true
+    elsif insurance_enroll == "n"
+      insurance_enroll = false
+    else
+      abort "Please choose (y or n)"
+    end
 
-
-  #Vampire detection logic 
+  Vampire detetion logic
 
   age_right = 2016 - year_born - age.abs <= 1
 
   if name == "Drake Cula" || name == "Tu Fang"
-    puts "Definetely a vampire"
-  elsif !age_right && !garlic_eater && !insurance
-    puts "Almost certainly a vampire"
-  elsif !age_right && (!garlic_eater !insurance)
-    puts "Probably a vampire"
-  elsif age_right && (garlic_eater || insurance)
-    puts "Probably not a vampire"
+    puts "Definately a vampire."
+  elsif !age_right && !likes_garlic && !insurance_enroll 
+    puts "Almost certainly a vampire."
+  elsif !age_right && ( !likes_garlic || !insurance_enroll )
+    puts "Probably a vampire."
+  elsif age_right && (likes_garlic || insurance_enroll)
+    puts "Probably not a vampire."
   else
-    puts "Results inconcluvise"
+    puts "Results inconclusive"
   end
-  number_of_employees = number_of_employees - 1
+  num_of_employees = num_of_employees - 1
 end
-  
-  #Allergy detection
+
+
+
+
   
 
-  until response = gets.chomp
-    puts "Are there any allergies you could list? You can type done when finished."
-    response == "sunshine" || "done"
-  end
-  if response == "sunshine"
-    result = "Probably a vampire."
-  end
 
-  puts result
-  puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
