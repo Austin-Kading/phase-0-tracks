a0011e2a3fc82e3000dd62144c02ca3059a0021c
Austin Kading <austinekading@gmail.com>
2016-09-20 12:48:23 -0700

    Prompted user questions

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,48 +1,22 @@
-# Initializing empty hash.
+# INTERIOR DESIGN PROGRAM
+# INTERFACE for interior designer to input client data
+# get clients name, age, number of children, decor theme
+
+# Initializing client hash
 client = {}
 
-# Asking client for age.
-puts "What is your full name?"
+# Prompting user for information
+puts "What is your name?"
+client[:name] = gets.chomp.to_s
 
-# Clients input will update client name in hash.
-client[:full_name] = gets.chomp
-
-# Promp user for age 
 puts "What is your age?"
-
-# Assigns user input to age symbol in client hash.
 client[:age] = gets.chomp.to_i
 
-# Prompt user for number of children.
 puts "How many children do you have?"
+client[:num_of_children] = gets.chomp.to_i
 
-# Number of children is added to symbol in client hash.
-client[:children] = gets.chomp.to_i
+puts "What is your favorite decor theme?"
+client[:dec_theme] = gets.chomp
 
-# Asking client if they like wood decor.
-puts "Do you like wood decor.(y/n)"
-
-# Converts yes or no too boolean
-input = gets.chomp
-client[:likes_wood] = input == "y"
-
-
-
+# DRIVER CODE 
 p client
-#Asks the user if they would like to change a keyword
-puts "Which one of the answers would you like to change?"
-if ("none")
-  puts "Please choose a keyword to change"
-if input == "age" || input == "children"
-  client[input.to_sym] = gets.chomp.to_i
-elsif input == "name" || input == "decor"
-  client[input.to_sym] = gets.chomp
-else
-  client[input.to_sym] = gets.chomp == "y"
-  end
-  puts client
-
-end
-
-    
-
