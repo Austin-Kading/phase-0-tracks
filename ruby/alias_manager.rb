187b75ba6dabbd53681235259ebeeca3e32c5015
Austin Kading <austinekading@gmail.com>
2016-09-24 12:49:28 -0700

    added method that advances vowels to next vowel

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,8 +1,6 @@
 # Method that takes a real name and creates a fake name
-# Swapps the first and last name
-# Changes all the vowels to the next vowel in aeiou
-# and all of the consonents to the next consonant
 
+# Swapps the first and last name
 def swapp_name(name)
   reverse_name = name.split(' ')
   reverse_name.reverse!
@@ -10,5 +8,28 @@ def swapp_name(name)
   p new_name
 end
 
-swapp_name('Austin Kading')
+# Changes all the vowels to the next vowel in aeiou
+# and all of the consonents to the next consonant
+
+def next_vowel(str)
+  vowels = ["a", "e", "i", "o", "u"]
+
+  str = str.split('')
+  str_new = str.map do |char|
+    if vowels.include?(char)
+      vowels.rotate(1)[vowels.index(char)]
+    else
+      char
+    end
+  end
+  p str_new.join
+end
+
+
+
+
+# DRIVER CODE
+
+# swapp_name('Austin Kading')
+next_vowel('austin kading')
 
