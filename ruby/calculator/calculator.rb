2c51a68c71c02ea52c85d942c4834e6d7b5d390a
Austin Kading <austinekading@gmail.com>
2016-09-27 12:59:45 -0700

    added loop to user interface

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,17 +1,45 @@
-class Calculator
-  def add(x,y)
-    x + y
-  end
+# Write a calculate method that takes three parameters: an integer,
+# an operator string (example: "+"), and another integer.
 
-  def subtract(x,y)
-    x - y
-  end
-
-  def multiply(x,y)
-    x * y
-  end
-
-  def divide(x,y)
-    x / y
+def calculate(a, operator, b)
+  if operator == '+'
+     a + b
+  elsif operator == '*'
+     a * b
+  elsif operator == '/'
+     a / b
+  elsif operator == '-'
+     a - b
+  else
+    puts 'Please try another problem.'
   end
 end
+
+# p calculate(4, '+', 5)
+# p calculate(4, '-', 5)
+# p calculate(4, '/', 5)
+# p calculate(4, '*', 5)
+
+# CREATE USER INTERFACE
+
+
+# I can't figure out how to get my loop to break properly
+# Going to bring it into office hours
+def request_calc
+  while true
+    puts "What calculation would you like to perform?"
+    puts "Type 'done' when you have finished."
+    operator = gets.chomp
+    puts "A #{operator} B. A = ?"
+    a = gets.chomp
+    puts "#{a} #{operator} B. B = ?"
+    b = gets.chomp
+    answer = calculate(a.to_i, operator, b.to_i)
+    puts "#{a} #{operator} #{b} = #{answer}"
+    false == "done"
+    break
+  end
+end
+
+p request_calc
+
