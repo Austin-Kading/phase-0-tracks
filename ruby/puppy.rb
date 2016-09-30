85454328faedd5af1a2a434301245ab635d3d9e3
Austin Kading <austinekading@gmail.com>
2016-09-29 18:23:52 -0700

    made puppy class with instance variables

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -4,7 +4,7 @@ class Puppy
   # Class variable that keeps track of how many animals there are in the class.
   @@no_of_animals = 0
   # Initializing multiple instances of the class
-    def initialize(name, color=nil, behavior, friendly)
+    def initialize(name, color, behavior, friendly)
       @name = name
       @color = color
       @behavior = behavior
@@ -14,5 +14,6 @@ end
 
 # Assigning instances to Benji the dog
 
-Benji = Puppy.new("Benji","brown", "playful", "yes")
-
+p benji = Puppy.new("Benji","brown", "playful", true)
+p fido = Puppy.new("fido","red", "playful", false)
+p spot = Puppy.new("spot","white", "playful", true)
