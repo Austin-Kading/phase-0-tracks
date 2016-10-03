bf8326a93d1251f9585c1f9840ddf715c56d5007
Austin Kading <austinekading@gmail.com>
2016-10-02 23:38:56 -0700

    made instance methods for game class

--- ruby/hangman_game/hangman.rb
+++ ruby/hangman_game/hangman.rb
@@ -1,12 +1,61 @@
 
 class Hangman
-  attr_reader :guess_count
-  attr_reader :word
+  attr_reader :guess_count, :is_over, :progress
+  attr_accessor :word
  
- def initialize
-  @word = "banjo"
-  @guess_count = 0
-  @is_over = false
- end
+   def initialize
+    @word = []
+    @guess_count = 0
+    @is_over = false
+    @progress = "_" 
+   
+   end
 
+   def game_word(choice_word)
+    @word = choice_word.downcase.split('')
+   end
+
+  def check_letter(char)
+    @guess_count += 1
+     if @word.include?(char)
+      puts "The word contains #{char}."
+     else
+    puts "wrong letter try again."
+    end
+  end
+
+  def current_state
+    @progress * @word.length
+  end
 end
+
+   def num_of_guesses
+    max_guesses = @word.length
+   end
+
+   
+
+   
+
+
+
+
+# USER INTERFACE
+
+# puts "Welcome to the hangman game!"
+game = Hangman.new
+
+# puts "What word do you choose for your opponent to guess?"
+# game_word = game.user_word(gets.chomp.downcase)
+
+ # p game.random_choice
+  p game.game_word("austin")
+  p game.current_state
+  #  game.word
+  # p game.check_letter("a")
+  # p game.current_state
+  # p game.guess_count
+ 
+
+# game = Hangman.new
+# p game.user_word("katie")
--- ruby/hangman_game/hangman_spec.rb
+++ ruby/hangman_game/hangman_spec.rb
@@ -4,6 +4,10 @@ describe Hangman do
   let(:hangman) { Hangman.new }
 
   it "Lets user enter a word or phrase" do
-    expect(hangman.word).to eq "banjo"
+    expect(hangman.game_word("austin")).to eq "austin"
+  end
+
+  it "Checks to see if the word contains a letter" do
+    expect(hangman.check_letter("a")).to eq "a"
   end
 end
