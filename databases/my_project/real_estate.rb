efa86622b2a573f281e07cf391483aadf8266ce4
Austin Kading <austinekading@gmail.com>
2016-10-17 10:07:57 -0700

    added client table

--- databases/my_project/real_estate.rb
+++ databases/my_project/real_estate.rb
@@ -12,3 +12,14 @@ create_locations_table = <<-SQL
     area VARCHAR(255)
   )
 SQL
+
+# If one doesn't already exist, create a client table with id, name, area desired and max price
+create_clients_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS clients(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    max_price INT,
+    area_id INT,
+    FOREIGN KEY (area_id) REFERENCES locations(id)
+  )
+SQL
